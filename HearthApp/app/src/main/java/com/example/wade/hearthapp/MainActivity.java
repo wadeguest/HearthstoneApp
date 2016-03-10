73efd29516398d96cd78c76661862c1c57b6443e
wadeguest <wade.guest@hotmail.ca>
2016-03-09 21:09:22 -0600

    test

--- HearthApp/app/src/main/java/com/example/wade/hearthapp/MainActivity.java
+++ HearthApp/app/src/main/java/com/example/wade/hearthapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.wade.hearthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
